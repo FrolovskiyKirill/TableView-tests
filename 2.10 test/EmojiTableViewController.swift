d439b5cbf3042758144a1980039c1eb8c9105a8a
Kirill <frolkv@gmail.com>
2022-05-06 22:45:19 +0300

    v 0.31

--- 2.10 test/EmojiTableViewController.swift
+++ 2.10 test/EmojiTableViewController.swift
@@ -23,15 +23,13 @@ class EmojiTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return objects.count
     }
 
