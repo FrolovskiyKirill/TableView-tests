50372fc2f228c8d054a7d0af246db4c4c4d47eef
Kirill <frolkv@gmail.com>
2022-05-06 13:15:27 +0300

    0.1

---
+++ 2.10 test/EmojiModel.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiModel.swift
+//  2.10 test
+//
+//  Created by Кирилл on 06.05.2022.
+//
+
+import Foundation
+
+struct EmojiModel {
+    
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavorite: Bool
+    
+}
---
+++ 2.10 test/EmojiTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiTableViewCell.swift
+//  2.10 test
+//
+//  Created by Кирилл on 06.05.2022.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    
+    @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
